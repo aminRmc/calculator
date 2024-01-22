f66f7c7d2ce03ba74b1023225ee93835e0cdf0b0
aminRmc <ramcilovicamin@gmail.com>
2024-01-22 16:32:20 +0100

    add commit

--- src/main.js
+++ src/main.js
@@ -9,36 +9,33 @@ let clear = document.querySelector(`#clear`)
 
 number.forEach(element => {
     element.addEventListener(`click`,()=>{
-        if (num == "" && ["+", "*", "/",".","-"].includes(element.value)) {
+        if (num == "" && ["+", "*", "/",".","-"].includes(element.value)) { //verfie for the first tap 
             return}
     
            
-        if (["+", "-", "*", "/","."].includes(num.slice(-1)) && ["+", "-", "*", "/","."].includes(element.value)) {
+        if (["+", "-", "*", "/","."].includes(num.slice(-1)) && ["+", "-", "*", "/","."].includes(element.value)) { //verifie if we have a operator 
             return;}
             else{
-       
+       //else do a normal calcul
         num += element.value
         sceen.value = num
     }
     })
-    if (num == sceen.value   && ["+", "*", "/",".","-"].includes(element.value)) {
-         sceen.value = num 
-    }
 });
 
 egal.addEventListener(`click`,()=>{
-    if (sceen.value == `` && ["+", "*", "/",".","-","="].includes(element.value)) {
+    if (sceen.value == `` && ["+", "*", "/",".","-","="].includes(element.value)) { // verify if we have string in screen
         sceen.value = ``
     }
 
-    else{
+    else{   //do a calcul
     calc =  Function("return " + num)();
     sceen.value = calc
     num = ``}
     
 })
 
-clear.addEventListener('click', ()=>{   
+clear.addEventListener('click', ()=>{    // clear screen
     sceen.value = ``
     num = ``
 
